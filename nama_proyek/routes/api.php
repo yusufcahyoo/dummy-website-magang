5a4cc5b7ef99d510a00f5573a9a00536f1486ab2
Zidan <zidan@gmail.com>
2025-03-14 14:56:07 +0700

    mengembalikan semula

--- nama_proyek/routes/api.php
+++ nama_proyek/routes/api.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user/slims/perpus/update/llll/asda', function (
-    Request $request
-) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
