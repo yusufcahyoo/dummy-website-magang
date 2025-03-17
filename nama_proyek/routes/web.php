f1edf2521aa5d8455f482c20b61c5275eb789ed2
unknown <aalgeraldi65@gmail.com>
2025-03-17 12:15:17 +0700

    nambah perpus

--- nama_proyek/routes/web.php
+++ nama_proyek/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/welcome/perpus', function () {
     return view('welcome');
 });
